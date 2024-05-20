cb1c19bae7fae2e17e0f46e14c38de1a82661007
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-05-20 12:15:10 +0700

    UI_commit01_khoa: complete login + registration form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import LoginForm from './Components/LoginForm/LoginForm';
+// import LoginForm from './Components/LoginForm/LoginForm';
+import LoginRegister from './Components/LoginRegister/LoginRegister';
 function App() {
   return (
     <div >
-      <LoginForm/>
+      <LoginRegister />
     </div>
   );
 }
